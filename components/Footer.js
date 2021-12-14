c5db82f18eefc8f805705bc69773edcbb7039a04
Yuji Tani <>
2021-12-14 23:52:53 +0900

    gitの管理下からsetting.jsonを除外。CSSモジュールをコンポーネント毎に切り分け

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,17 @@
-import styles from '../styles/Home.module.css'
+import classes from './Footer.module.css'
+import Image from 'next/image'
 
 export function Footer() {
   return (
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+    <footer className={classes.footer}>
+      <a
+        href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Powered by{' '}
+        <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
+      </a>
+    </footer>
   )
 }
