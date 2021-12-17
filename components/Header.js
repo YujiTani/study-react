2bc2bca73059253bf5ecd8528bb877dd7d8f49d9
Yuji Tani <>
2021-12-17 10:05:37 +0900

    ヘッダーコンポーネントを追加

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import classes from './Header.module.css'
+import Link from 'next/link'
+
+export function Header() {
+  return (
+    <header className={classes.header}>
+      <Link href='/' prefetch={false}>
+        index
+      </Link>
+      <Link href='/about'>about</Link>
+    </header>
+  )
+}
