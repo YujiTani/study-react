306f87864aec1a1ed095a4c6500629061aac2d26
Yuji Tani <>
2021-12-17 10:06:35 +0900

    ♻️ リンクスコンポーネントをリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -1,33 +1,43 @@
 import classes from './Links.module.css'
 
+const ITEMS = [
+  {
+    id: 1,
+    href: 'https://nextjs.org/docs',
+    title: 'Documentation →',
+    description: 'Find in-depth information about Next.js features and API.',
+  },
+  {
+    id: 2,
+    href: 'https://nextjs.org/learn',
+    title: 'Learn →',
+    description: 'Learn about Next.js in an interactive course with quizzes!',
+  },
+  {
+    id: 3,
+    href: 'https://github.com/vercel/next.js/tree/master/examples',
+    title: 'Examples →',
+    description: 'Discover and deploy boilerplate example Next.js projects.',
+  },
+  {
+    id: 4,
+    href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Deploy →',
+    description:
+      'Instantly deploy your Next.js site to a public URL with Vercel.',
+  },
+]
 export function Links() {
   return (
     <div className={classes.grid}>
-      <a href='https://nextjs.org/docs' className={classes.card}>
-        <h2>Documentation &rarr;</h2>
-        <p>Find in-depth information about Next.js features and API.</p>
-      </a>
-
-      <a href='https://nextjs.org/learn' className={classes.card}>
-        <h2>Learn &rarr;</h2>
-        <p>Learn about Next.js in an interactive course with quizzes!</p>
-      </a>
-
-      <a
-        href='https://github.com/vercel/next.js/tree/master/examples'
-        className={classes.card}
-      >
-        <h2>Examples &rarr;</h2>
-        <p>Discover and deploy boilerplate example Next.js projects.</p>
-      </a>
-
-      <a
-        href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-        className={classes.card}
-      >
-        <h2>Deploy &rarr;</h2>
-        <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-      </a>
+      {ITEMS.map((item) => {
+        return (
+          <a href={item.href} className={classes.card} key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+          </a>
+        )
+      })}
     </div>
   )
 }
