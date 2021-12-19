d2d781468e8d71389bc483ee502f563f1249ac7b
Yuji Tani <>
2021-12-19 12:19:07 +0900

    💫 ボタンをメインに持たせるように変更

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,14 +1,22 @@
 import classes from '@/components/Main/Main.module.css'
 import { Headline } from '@/components/Headline'
 import { Links } from '@/components/Links'
+import { useCallback } from 'react'
 
 export function Main(props) {
+  const handleClick = useCallback(function (e) {
+    console.log(location.href)
+    e.preventDefault()
+  }, [])
+
   return (
     <main className={classes.main}>
       <Headline page={props.page}>
         <code className={classes.code}>pages/{props.page}.js</code>
       </Headline>
-      <button className={classes.btn}>ボタン</button>
+      <button className={classes.btn} onClick={handleClick}>
+        現在のURL表示
+      </button>
       <Links />
     </main>
   )
