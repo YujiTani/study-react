feee59bcaa5bbb525cb9e1cb8be11ce0d1e6a88f
Yuji Tani <>
2021-12-21 10:13:09 +0900

    💫 useStateを導入し、動的に変化する値を作った。

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,21 +1,36 @@
 import classes from '@/components/Main/Main.module.css'
 import { Headline } from '@/components/Headline'
 import { Links } from '@/components/Links'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 
 export function Main(props) {
+  const [count, setCount] = useState(1)
+
   const handleClick = useCallback(function (e) {
-    console.log(location.href)
-    e.preventDefault()
+    alert(`現在のURLは、${location.href}`)
   }, [])
+
+  const handleCount = useCallback(
+    function () {
+      if (count > 9) {
+        return
+      }
+      setCount((count) => count + 1)
+    },
+    [count]
+  )
 
   return (
     <main className={classes.main}>
       <Headline page={props.page}>
         <code className={classes.code}>pages/{props.page}.js</code>
       </Headline>
+      <h1>{count}</h1>
       <button className={classes.btn} onClick={handleClick}>
-        現在のURL表示
+        ボタン
+      </button>
+      <button className={classes.btn} onClick={handleCount}>
+        カウントアップ
       </button>
       <Links />
     </main>
