a5449914642d526ea03b6eb3fceefae8195b998a
Yuji Tani <>
2021-12-22 23:29:02 +0900

    💫 useStateを使った関数を２つ追加

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -5,6 +5,8 @@ import { useCallback, useState } from 'react'
 
 export function Main(props) {
   const [count, setCount] = useState(1)
+  const [text, setText] = useState('')
+  const [isShow, setIsShow] = useState(true)
 
   const handleClick = useCallback(function (e) {
     alert(`現在のURLは、${location.href}`)
@@ -20,11 +22,29 @@ export function Main(props) {
     [count]
   )
 
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow)
+  }, [])
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      return
+    }
+    setText((text) => e.target.value)
+  }, [])
+
   return (
     <main className={classes.main}>
       <Headline page={props.page}>
         <code className={classes.code}>pages/{props.page}.js</code>
       </Headline>
+      {isShow ? (
+        <label htmlFor='input'>
+          name
+          <input id='input' type='text' value={text} onChange={handleChange} />
+        </label>
+      ) : null}
+      <button onClick={handleDisplay}>{isShow ? '非表示' : '表示'}</button>
       <h1>{count}</h1>
       <button className={classes.btn} onClick={handleClick}>
         ボタン
@@ -32,6 +52,7 @@ export function Main(props) {
       <button className={classes.btn} onClick={handleCount}>
         カウントアップ
       </button>
+
       <Links />
     </main>
   )
