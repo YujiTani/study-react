a15f2d898034bae8e51dd01e745420c27d9107b9
Yuji Tani <tani@leanonme.co.jp>
2022-01-24 23:45:01 +0900

    コメント一覧ページをSG化

--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -1,15 +1,43 @@
 import Head from 'next/head'
 import { Header } from '@/components/Header'
 import { CommentsComponent } from '@/components/Comment'
+import { SWRConfig } from 'swr'
 
-const Comments = () => {
+const sleep = async(ms) => new Promise((resolve) => setTimeout(resolve , ms))
+
+// ServerSideProps
+// export const getServerSideProps = async() => {
+//   const COMMENTS_API_URL = "https://jsonplaceholder.typicode.com/comments"
+//   const comments = await fetch(COMMENTS_API_URL)
+//   const commentsData = await comments.json();
+//   await sleep(2000)
+
+  export const getStaticProps = async() => {
+    const COMMENTS_API_URL = "https://jsonplaceholder.typicode.com/comments"
+    const comments = await fetch(COMMENTS_API_URL)
+    const commentsData = await comments.json();
+    await sleep(2000)
+
+  return {
+    props: {
+      fallback : {
+        [COMMENTS_API_URL]: commentsData,
+      },
+    },
+  }
+}
+
+const Comments = (props) => {
+  const { fallback } = props
   return (
     <div>
       <Head>
         <title>Comments Page</title>
       </Head>
+      <SWRConfig value={{ fallback }}>
       <Header />
       <CommentsComponent />
+      </SWRConfig>
     </div>
   )
 }
